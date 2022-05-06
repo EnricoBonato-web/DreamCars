46bb47b7fe687e7e144825432b17049156d30b29
EnricoBonatoBan <101053509+EnricoBonatoBan@users.noreply.github.com>
2022-05-06 12:53:46 +0200

    Update mappa.js

--- Javascript/mappa.js
+++ Javascript/mappa.js
@@ -5,7 +5,7 @@ document.getElementById("googleMap").style.height='300px';
     center:new google.maps.LatLng(45.411331,11.8854431),
     zoom:14 ,
 };
- src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAaUkxp5oyLvwG5UI0jY5EmMg2InF-uMUA&callback=myMap"
+ src="https://maps.googleapis.com/maps/api/js?key=API_KEY&callback=myMap"
 
     var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
  var marker = new google.maps.Marker({
